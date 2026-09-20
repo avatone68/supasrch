5874d7ce726f1d63f9d1beaa77dd9456a7e4d2e8
avatone68 <32532303+avatone68@users.noreply.github.com>
2026-09-20 17:38:25 +1000

    Update build.gradle.kts

--- SupaSearch/build.gradle.kts
+++ SupaSearch/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
 }
 
-version = 1
+version = 2
 
 cloudstream {
     description = "Merges search results from all installed providers"
